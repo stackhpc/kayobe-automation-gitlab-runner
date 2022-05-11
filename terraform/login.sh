9d1e9a29dbed50a9f364964a1bfa5beba3f6e348
Will Szumski <will@stackhpc.com>
2022-05-11 13:41:34 +0100

    Adds GITLAB_USER environment variable

--- terraform/login.sh
+++ terraform/login.sh
@@ -3,11 +3,13 @@
 set -eux
 state_file=$1
 
+GITLAB_USER="${GITLAB_USER:-will70}"
+
 terraform init \
     -backend-config="address=https://gitlab.com/api/v4/projects/28023768/terraform/state/$state_file" \
     -backend-config="lock_address=https://gitlab.com/api/v4/projects/28023768/terraform/state/$state_file/lock" \
     -backend-config="unlock_address=https://gitlab.com/api/v4/projects/28023768/terraform/state/$state_file/lock" \
-    -backend-config="username=will70" \
+    -backend-config="username=$GITLAB_USER" \
     -backend-config="password=$GITLAB_TOKEN" \
     -backend-config="lock_method=POST" \
     -backend-config="unlock_method=DELETE" \

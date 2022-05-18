cd682744e7c7a8ef336c61efd466679fdd34c8df
Will Szumski <will@stackhpc.com>
2022-05-18 13:33:28 +0100

    Make project configurable

--- terraform/login.sh
+++ terraform/login.sh
@@ -4,11 +4,12 @@ set -eux
 state_file=$1
 
 GITLAB_USER="${GITLAB_USER:-will70}"
+GITLAB_PROJECT="${GITLAB_PROJECT:-28023768}"
 
 terraform init \
-    -backend-config="address=https://gitlab.com/api/v4/projects/28023768/terraform/state/$state_file" \
-    -backend-config="lock_address=https://gitlab.com/api/v4/projects/28023768/terraform/state/$state_file/lock" \
-    -backend-config="unlock_address=https://gitlab.com/api/v4/projects/28023768/terraform/state/$state_file/lock" \
+    -backend-config="address=https://gitlab.com/api/v4/projects/$GITLAB_PROJECT/terraform/state/$state_file" \
+    -backend-config="lock_address=https://gitlab.com/api/v4/projects/$GITLAB_PROJECT/terraform/state/$state_file/lock" \
+    -backend-config="unlock_address=https://gitlab.com/api/v4/projects/$GITLAB_PROJECT/terraform/state/$state_file/lock" \
     -backend-config="username=$GITLAB_USER" \
     -backend-config="password=$GITLAB_TOKEN" \
     -backend-config="lock_method=POST" \
